e71352f01b304cd40e1e48b96f248955208d4332
SushmaKumari777 <sushmapriya271005@gmail.com>
2025-09-29 11:58:28 +0530

    change

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import { Col, Container, Row, Button } from 'react-bootstrap'
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import './register.css'
 import InputMask from 'react-input-mask';
 import Menu from './Menu'
+import { register } from "../slices/auth";
+import { clearMessage } from "../slices/message";
+import { useDispatch, useSelector } from "react-redux";
+
 
 const SignupSchema = Yup.object().shape({
     mobile: Yup.string()
-      .matches(/^[6-9]\d{9}$/, "Enter a valid 10 Digit Mobile No. ")
-      .required("Mobile No. is Mandetory!"),
+        .matches(/^[6-9]\d{9}$/, "Enter a valid 10 Digit Mobile No. ")
+        .required("Mobile No. is Mandetory!"),
     email: Yup.string().email('Invalid email').required('E-Mail Id Required'),
     password: Yup.string()
         .min(6, 'Minimum Six charecter required')
@@ -17,6 +21,28 @@ const SignupSchema = Yup.object().shape({
         .required('Password Required'),
 });
 const Register = () => {
+    const [successful, setSuccessful] = useState(false);
+
+    const { message } = useSelector((state) => state.message);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(clearMessage());
+    }, [dispatch]);
+    const handleRegister = (formValue) => {
+        const { username, email, password } = formValue;
+
+        setSuccessful(false);
+
+        dispatch(register({ username, email, password }))
+            .unwrap()
+            .then(() => {
+                setSuccessful(true);
+            })
+            .catch(() => {
+                setSuccessful(false);
+            });
+    };
     return (
         <div>
             <section>
@@ -41,10 +67,11 @@ const Register = () => {
                                     password: '',
                                 }}
                                 validationSchema={SignupSchema}
-                                onSubmit={values => {
+                                // onSubmit={values => {
                                     // same shape as initial values
-                                    console.log(values);
-                                }}
+                                    // console.log(values);
+                                // }}
+                                onSubmit={handleRegister}
                             >
                                 {({ errors, touched }) => (
                                     <Form>
@@ -52,11 +79,11 @@ const Register = () => {
                                             <Row>
                                                 <Col md={3}><b>Mobile</b></Col>
                                                 <Col md={9}>
-                                                    <Field name="mobile" className='bbb' /> 
+                                                    <Field name="mobile" className='bbb' />
                                                     {
                                                         /*<InputMask mask="9999999999" maskChar={null} name="mobile" className='boxes' /> */
                                                     }
-                                                    
+
 
                                                     <div className="error">
                                                         {errors.mobile && touched.mobile ? (
